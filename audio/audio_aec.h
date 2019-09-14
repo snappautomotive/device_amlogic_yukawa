49b0f4cae7c333083d6d0111c890a0f74f1bb0a4
Alden DSouza <aldend@google.com>
2019-09-13 18:55:13 -0700

    [Yukawa]: Integrate Google AEC into HAL
    Use google_aec library on mic capture path in HAL.
    
    Bug: 139423645
    Test: Manual
    Change-Id: If72c201bc30ce88dea6cb50a4ef436644bcf289c

---
+++ audio/audio_aec.h
@@ -0,0 +1,77 @@
+/*
+ * Copyright (C) 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef _YUKAWA_AUDIO_AEC_H_
+#define _YUKAWA_AUDIO_AEC_H_
+
+#include <stdint.h>
+#include <pthread.h>
+#include <sys/time.h>
+#include <hardware/audio.h>
+#include <audio_utils/resampler.h>
+#include "audio_hw.h"
+#include "fifo_wrapper.h"
+
+struct ts_fifo_payload {
+    struct timespec tstamp;
+    unsigned int avail;
+    ssize_t bytes;
+};
+
+struct aec_itfe {
+    pthread_mutex_t lock;
+    int32_t *mic_buf;
+    ssize_t mic_buf_size_bytes;
+    ssize_t mic_frame_size;
+    uint32_t mic_sampling_rate;
+    struct ts_fifo_payload last_mic_ts;
+    int32_t *spk_buf;
+    ssize_t spk_buf_size_bytes;
+    ssize_t spk_frame_size;
+    uint32_t spk_sampling_rate;
+    struct ts_fifo_payload last_spk_ts;
+    int16_t *spk_buf_playback_format;
+    int16_t *spk_buf_resampler_out;
+    struct audio_fifo_itfe *spk_fifo;
+    struct audio_fifo_itfe *ts_fifo;
+    ssize_t read_write_diff_bytes;
+    struct resampler_itfe *spk_resampler;
+    bool spk_running;
+    bool prev_spk_running;
+};
+
+int write_to_spk_fifo (struct alsa_stream_out *out, void *buffer, size_t bytes);
+int process_aec (struct alsa_stream_in *stream, void* buffer, size_t bytes);
+int init_aec_spk_config (struct alsa_stream_out *out);
+int init_aec_mic_config (struct alsa_stream_in *in);
+void destroy_aec_spk_config (struct alsa_stream_out *out);
+void destroy_aec_mic_config (struct alsa_stream_in *in);
+int aec_set_spk_running (struct alsa_stream_out *out, bool state);
+
+#ifndef AEC_HAL
+
+#define write_to_spk_fifo(...) ((int)0)
+#define process_aec(...) ((int)0)
+#define init_aec_spk_config(...) ((int)0)
+#define init_aec_mic_config(...) ((int)0)
+#define destroy_aec_spk_config(...) ((int)0)
+#define destroy_aec_mic_config(...) ((int)0)
+#define aec_set_spk_running(...) ((int)0)
+
+#endif
+
+
+#endif /* _YUKAWA_AUDIO_AEC_H_ */
---
+++ audio/fifo_wrapper.cpp
@@ -0,0 +1,74 @@
+/*
+ * Copyright (C) 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#define LOG_TAG "audio_utils_fifo_wrapper"
+// #define LOG_NDEBUG 0
+
+#include <stdint.h>
+#include <errno.h>
+#include <log/log.h>
+#include <audio_utils/fifo.h>
+#include "fifo_wrapper.h"
+
+int fifo_init(struct audio_fifo_itfe **p_fifo_itfe, uint32_t bytes, bool reader_throttles_writer) {
+    struct audio_fifo_itfe *fifo_itfe = new struct audio_fifo_itfe;
+    fifo_itfe->p_buffer = new int8_t[bytes];
+    if (fifo_itfe->p_buffer == NULL) {
+        ALOGE("Failed to allocate fifo buffer!");
+        return -ENOMEM;
+    }
+    audio_utils_fifo *p_fifo = new audio_utils_fifo(bytes, 1, fifo_itfe->p_buffer, reader_throttles_writer);
+    fifo_itfe->p_fifo = static_cast<void*>(p_fifo);
+    fifo_itfe->p_fifo_writer = static_cast<void*>(new audio_utils_fifo_writer(*p_fifo));
+    fifo_itfe->p_fifo_reader = static_cast<void*>(new audio_utils_fifo_reader(*p_fifo));
+
+    *p_fifo_itfe = fifo_itfe;
+
+    return 0;
+}
+
+void fifo_release(struct audio_fifo_itfe *fifo_itfe) {
+    delete static_cast<audio_utils_fifo_writer *>(fifo_itfe->p_fifo_writer);
+    delete static_cast<audio_utils_fifo_reader *>(fifo_itfe->p_fifo_reader);
+    delete static_cast<audio_utils_fifo *>(fifo_itfe->p_fifo);
+    delete[] fifo_itfe->p_buffer;
+    delete fifo_itfe;
+}
+
+ssize_t fifo_read(struct audio_fifo_itfe *fifo_itfe, void *buffer, size_t bytes) {
+    audio_utils_fifo_reader *p_fifo_reader = static_cast<audio_utils_fifo_reader *>(fifo_itfe->p_fifo_reader);
+    return p_fifo_reader->read(buffer, bytes);
+}
+
+ssize_t fifo_write(struct audio_fifo_itfe *fifo_itfe, void *buffer, size_t bytes) {
+    audio_utils_fifo_writer *p_fifo_writer = static_cast<audio_utils_fifo_writer *>(fifo_itfe->p_fifo_writer);
+    return p_fifo_writer->write(buffer, bytes);
+}
+
+ssize_t fifo_available_to_read(struct audio_fifo_itfe *fifo_itfe) {
+    audio_utils_fifo_reader *p_fifo_reader = static_cast<audio_utils_fifo_reader *>(fifo_itfe->p_fifo_reader);
+    return p_fifo_reader->available();
+}
+
+ssize_t fifo_available_to_write(struct audio_fifo_itfe *fifo_itfe) {
+    audio_utils_fifo_writer *p_fifo_writer = static_cast<audio_utils_fifo_writer *>(fifo_itfe->p_fifo_writer);
+    return p_fifo_writer->available();
+}
+
+ssize_t fifo_flush(struct audio_fifo_itfe *fifo_itfe) {
+    audio_utils_fifo_reader *p_fifo_reader = static_cast<audio_utils_fifo_reader *>(fifo_itfe->p_fifo_reader);
+    return p_fifo_reader->flush();
+}
