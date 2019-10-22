cc055d3657ed35709330b5482decc370a86261df
Guillaume La Roque <glaroque@baylibre.com>
2019-10-22 11:51:13 +0000

    yukawa: sei610: Add support of A/B
    only for sei610 board(yukawa).
    to build with A/B support :
    
       make all TARGET_USE_AB_SLOT=true
    
    can be used with AVB support too.
    
    switch of bootloader is needed, please flash u-boot_sei610_ab.bin:
    
      fastboot flash bootlaoder bootloader/u-boot_sei610_ab.bin
      fastboot reboot bootloader
      fastboot erase bootenv
      fastboot oem format
    
    when you flash bins please use slot suffix for first time:
    fastboot flash boot_a ...
    fastboot flash vbmeta_a ... (if avb true)
    
    Change-Id: I0276d4fb53859f67f0491800633614e572bdf1de
    Signed-off-by: Guillaume La Roque <glaroque@baylibre.com>

--- yukawa/BoardConfig.mk
+++ yukawa/BoardConfig.mk
@@ -4,6 +4,10 @@ ifeq ($(TARGET_VIM3L),)
 DEVICE_MANIFEST_FILE += device/amlogic/yukawa/yukawa/manifest.xml
 endif
 
-BOARD_USERDATAIMAGE_PARTITION_SIZE := 13416529920
-
 TARGET_BOARD_INFO_FILE := device/amlogic/yukawa/sei610/board-info.txt
+
+ifeq ($(TARGET_USE_AB_SLOT), true)
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 11846811648
+else
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 13415481344
+endif
